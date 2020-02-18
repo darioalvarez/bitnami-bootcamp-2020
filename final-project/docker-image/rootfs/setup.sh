21aeb2ce029510c48f86f62e8771f93fad64fa47
Dario Alvarez Arteaga <daa.developer@gmail.com>
2020-02-18 11:54:01 +0100

    Solving minor issue detected on database initialization logic.

--- final-project/docker-image/rootfs/setup.sh
+++ final-project/docker-image/rootfs/setup.sh
@@ -2,7 +2,6 @@
 
 sleep 10
 
-
 PGPASSWORD=password123 psql -h ${POSTGRESQL_HOST} -U postgres -c \
 "CREATE DATABASE ${POSTGRESQL_CKAN_DATASTORE};"
 
@@ -29,18 +28,11 @@ PGPASSWORD=password123 psql -h ${POSTGRESQL_HOST} -U postgres -c \
 "GRANT ALL PRIVILEGES ON DATABASE ${POSTGRESQL_CKAN_DATABASE} TO ${POSTGRESQL_DATABASE_USER};"
 
 
-
-# Step 7 !!!!!!! init database
 cd /usr/lib/ckan/default/src/ckan
 . /usr/lib/ckan/default/bin/activate
 
-if [[ "${CKAN_INITIALIZE_DB}" = "yes"  ]]; then
-    paster make-config ckan /etc/ckan/default/development.ini
-fi
+paster make-config ckan /etc/ckan/default/development.ini
 
-
-
-# Edit development.ini file
 sed -i "s!site_id=default!site_id=${CKAN_SITE_ID}!1" /etc/ckan/default/development.ini
 sed -i "s!sqlalchemy.url = postgresql://ckan_default:pass@localhost/ckan_default!sqlalchemy.url = postgresql://${POSTGRESQL_DATABASE_USER}:${POSTGRESQL_DATABASE_USER_PASSWORD}@${POSTGRESQL_HOST}/${POSTGRESQL_CKAN_DATABASE}!1" /etc/ckan/default/development.ini
 sed -i "s!ckan.site_url =!ckan.site_url = ${CKAN_HOST}:${CKAN_PORT}!1" /etc/ckan/default/development.ini
@@ -51,15 +43,11 @@ sed -i "s!ckan.plugins = stats text_view image_view recline_view!ckan.plugins = 
 sed -i "s!#ckan.datastore.write_url = postgresql://ckan_default:pass@localhost/datastore_default!ckan.datastore.write_url = postgresql://${POSTGRESQL_DATASTORE_USER_WRITE}:${POSTGRESQL_DATASTORE_USER_WRITE_PASSWORD}@${POSTGRESQL_HOST}/${POSTGRESQL_CKAN_DATASTORE}!1" /etc/ckan/default/development.ini
 sed -i "s!#ckan.datastore.read_url = postgresql://datastore_default:pass@localhost/datastore_default!ckan.datastore.read_url = postgresql://${POSTGRESQL_DATASTORE_USER_READ}:${POSTGRESQL_DATASTORE_USER_READ_PASSWORD}@${POSTGRESQL_HOST}/${POSTGRESQL_CKAN_DATASTORE}!1" /etc/ckan/default/development.ini
 
+if [[ "${CKAN_INITIALIZE_DB}" = "yes"  ]]; then
+   paster db init -c /etc/ckan/default/development.ini
 
-
-
-
-
-
-paster db init -c /etc/ckan/default/development.ini
-
-paster --plugin=ckan user add ${CKAN_ADMIN} email="${CKAN_ADMIN_EMAIL}" password="${CKAN_ADMIN_PASSWORD}" -c /etc/ckan/default/development.ini
-paster --plugin=ckan sysadmin add ${CKAN_ADMIN} -c /etc/ckan/default/development.ini
+   paster --plugin=ckan user add ${CKAN_ADMIN} email="${CKAN_ADMIN_EMAIL}" password="${CKAN_ADMIN_PASSWORD}" -c /etc/ckan/default/development.ini
+    paster --plugin=ckan sysadmin add ${CKAN_ADMIN} -c /etc/ckan/default/development.ini
+fi
 
 deactivate
